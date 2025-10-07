a8c9f93a038b04e3481c8d7155ddf0b7bcb7544a
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2025-10-07 15:09:53 -0300

    Substituindo comando para executar todos os containers pelo comando para parar todos

--- Mastodon/ConfiguracaoInicial.bash
+++ Mastodon/ConfiguracaoInicial.bash
@@ -3,4 +3,4 @@ docker compose up -d mastodon_db mastodon_redis
 docker compose run --rm mastodon_web bundle exec rails db:setup
 docker compose run --rm mastodon_web bundle exec rails db:migrate
 docker compose run --rm mastodon_web bundle exec rails assets:precompile
-docker compose up
+docker compose down
