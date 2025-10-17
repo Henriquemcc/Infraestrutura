42b0c90bac9e11e83ab1e7c348cdc1d5e7aabe71
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2025-10-16 22:33:23 -0300

    Corrigindo erro ao iniciar o CoreDNS

--- CoreDNS/GerarCorefile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-. ./.env
-export BASE_DOMAIN
-envsubst < Corefile.template > Corefile
--- CoreDNS/entrypoint.sh
+++ CoreDNS/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -e
 
+# Criando arquivo CoreDNS
+envsubst < /etc/coredns/Corefile.template > /etc/coredns/Corefile
+
 # Copia certificados para um diretório com permissões mais abertas
 mkdir -p /etc/certs
-cp /etc/letsencrypt/live/mydomain.com/fullchain.pem /etc/certs/
-cp /etc/letsencrypt/live/mydomain.com/privkey.pem /etc/certs/
+cp /etc/letsencrypt/live/${BASE_DOMAIN}/fullchain.pem /etc/certs/
+cp /etc/letsencrypt/live/${BASE_DOMAIN}/privkey.pem /etc/certs/
 chmod 644 /etc/certs/fullchain.pem
 chmod 600 /etc/certs/privkey.pem
 
 # Executa o CoreDNS
-exec /coredns -conf /etc/coredns/Corefile
+exec coredns -conf /etc/coredns/Corefile
