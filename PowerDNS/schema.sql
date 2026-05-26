789cc40a359aa433a0b8497761a448d32e49a881
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-26 16:31:06 -0300

    Corrigindo esquema do banco de dados

--- PowerDNS/schema.sql
+++ PowerDNS/schema.sql
@@ -1,42 +1,47 @@
+-- https://doc.powerdns.com/authoritative/backends/generic-mysql.html#default-schema
 CREATE TABLE domains (
   id                    INT AUTO_INCREMENT,
   name                  VARCHAR(255) NOT NULL,
   master                VARCHAR(128) DEFAULT NULL,
   last_check            INT DEFAULT NULL,
-  type                  VARCHAR(32) NOT NULL,
+  type                  VARCHAR(8) NOT NULL,
   notified_serial       INT UNSIGNED DEFAULT NULL,
   account               VARCHAR(40) CHARACTER SET 'utf8' DEFAULT NULL,
+  options               VARCHAR(64000) DEFAULT NULL,
   catalog               VARCHAR(255) DEFAULT NULL,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine=InnoDB CHARACTER SET 'latin1';
 
 CREATE UNIQUE INDEX name_index ON domains(name);
 CREATE INDEX catalog_idx ON domains(catalog);
+
 
 CREATE TABLE records (
   id                    BIGINT AUTO_INCREMENT,
   domain_id             INT DEFAULT NULL,
   name                  VARCHAR(255) DEFAULT NULL,
   type                  VARCHAR(10) DEFAULT NULL,
-  content               TEXT DEFAULT NULL,
+  content               VARCHAR(64000) DEFAULT NULL,
   ttl                   INT DEFAULT NULL,
   prio                  INT DEFAULT NULL,
   disabled              TINYINT(1) DEFAULT 0,
   ordername             VARCHAR(255) BINARY DEFAULT NULL,
   auth                  TINYINT(1) DEFAULT 1,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine=InnoDB CHARACTER SET 'latin1';
 
 CREATE INDEX nametype_index ON records(name,type);
 CREATE INDEX domain_id ON records(domain_id);
-CREATE INDEX ordername ON records(ordername);
+CREATE INDEX ordername ON records (ordername);
+
 
 CREATE TABLE supermasters (
   ip                    VARCHAR(64) NOT NULL,
   nameserver            VARCHAR(255) NOT NULL,
   account               VARCHAR(40) CHARACTER SET 'utf8' NOT NULL,
   PRIMARY KEY (ip, nameserver)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine=InnoDB CHARACTER SET 'latin1';
+
 
 CREATE TABLE comments (
   id                    INT AUTO_INCREMENT,
@@ -47,11 +52,11 @@ CREATE TABLE comments (
   account               VARCHAR(40) CHARACTER SET 'utf8' DEFAULT NULL,
   comment               TEXT CHARACTER SET 'utf8' NOT NULL,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine=InnoDB CHARACTER SET 'latin1';
 
-CREATE INDEX comments_domain_id_idx ON comments (domain_id);
 CREATE INDEX comments_name_type_idx ON comments (name, type);
 CREATE INDEX comments_order_idx ON comments (domain_id, modified_at);
+
 
 CREATE TABLE domainmetadata (
   id                    INT AUTO_INCREMENT,
@@ -59,21 +64,23 @@ CREATE TABLE domainmetadata (
   kind                  VARCHAR(32),
   content               TEXT,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine=InnoDB CHARACTER SET 'latin1';
 
 CREATE INDEX domainmetadata_idx ON domainmetadata (domain_id, kind);
+
 
 CREATE TABLE cryptokeys (
   id                    INT AUTO_INCREMENT,
   domain_id             INT NOT NULL,
   flags                 INT NOT NULL,
-  active                TINYINT(1),
-  published             TINYINT(1) DEFAULT 1,
+  active                BOOL,
+  published             BOOL DEFAULT 1,
   content               TEXT,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY(id)
+) Engine=InnoDB CHARACTER SET 'latin1';
 
 CREATE INDEX domainidindex ON cryptokeys(domain_id);
+
 
 CREATE TABLE tsigkeys (
   id                    INT AUTO_INCREMENT,
@@ -81,6 +88,6 @@ CREATE TABLE tsigkeys (
   algorithm             VARCHAR(50),
   secret                VARCHAR(255),
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine=InnoDB CHARACTER SET 'latin1';
 
-CREATE UNIQUE INDEX name_algo_index ON tsigkeys(name, algorithm);
+CREATE UNIQUE INDEX namealgoindex ON tsigkeys(name, algorithm);
