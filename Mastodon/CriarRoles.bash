80f765200e0050f1fb883e60dfff33c7893dffc3
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-08-01 21:16:44 -0300

    Transformando o Mastodon em um submódulo que utiliza o Kubernetes Chart oficial

--- Mastodon/CriarRoles.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Lendo variáveis de ambiente
-source .env
-
-# Criando roles
-docker exec -it "${MASTODON_WEB_CONTAINER_NAME}" bash -c "RAILS_ENV=production bin/rails runner 'UserRole.create!(name: \"Owner\") unless UserRole.exists?(name: \"Owner\"); UserRole.create!(name: \"Admin\") unless UserRole.exists?(name: \"Admin\"); UserRole.create!(name: \"Moderator\") unless UserRole.exists?(name: \"Moderator\"); UserRole.create!(name: \"User\") unless UserRole.exists?(name: \"User\")'"
