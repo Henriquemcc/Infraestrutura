7bba810019b6bc1462ce1a9569cca34f4e41a06d
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-08-07 08:11:14 -0300

    Removendo arquivos

--- Mastodon/CriarContaDono.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Lendo variáveis de ambiente
-source .env
-
-# Criando conta de dono
-docker exec -it "${MASTODON_WEB_CONTAINER_NAME}"  bash -c "RAILS_ENV=production; bin/tootctl accounts create \"${MASTODON_ADMIN_USERNAME}\" --email \"${MASTODON_ADMIN_EMAIL}\" --confirmed --role Owner"
